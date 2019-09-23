9b4c32ffa810608cf959f15faa034fe674873c1b
Rodnoy Konstantin <kotrod@gmail.com>
2019-09-23 16:02:04 +0300

    reject

--- model/src/main/java/com/github/cotrod/hotel/model/User.java
+++ model/src/main/java/com/github/cotrod/hotel/model/User.java
@@ -3,6 +3,7 @@ package com.github.cotrod.hotel.model;
 public class User {
     private String login;
     private String password;
+    private int age;
 
     public User(String login, String password) {
         this.login = login;
