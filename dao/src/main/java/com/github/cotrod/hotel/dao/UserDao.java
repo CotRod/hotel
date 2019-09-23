86d5745a5701c8e5460d1eb0f15be15f96d21af3
Rodnoy Konstantin <kotrod@gmail.com>
2019-09-23 15:50:00 +0300

    some fixes

--- dao/src/main/java/com/github/cotrod/hotel/dao/UserDao.java
+++ dao/src/main/java/com/github/cotrod/hotel/dao/UserDao.java
@@ -2,10 +2,7 @@ package com.github.cotrod.hotel.dao;
 
 import com.github.cotrod.hotel.model.User;
 
-import java.util.List;
-
 public interface UserDao {
-    List<User> getUsers();
 
     void save(User user);
 
