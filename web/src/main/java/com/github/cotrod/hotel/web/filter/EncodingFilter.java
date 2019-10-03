34ecc95905fa87149c2b5063c772232fc406e09f
Rodnoy Konstantin <kotrod@gmail.com>
2019-10-03 17:48:17 +0300

    tried to make a filter for encoding

---
+++ web/src/main/java/com/github/cotrod/hotel/web/filter/EncodingFilter.java
@@ -0,0 +1,20 @@
+package com.github.cotrod.hotel.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+//Не работает так, как хотелось... потом доделаю
+@WebFilter(urlPatterns = "/")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(request, response);
+    }
+}
