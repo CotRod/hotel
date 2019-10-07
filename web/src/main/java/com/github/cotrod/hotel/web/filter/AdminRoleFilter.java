4ad4ba5e531d02c3e118b3689b0685b069a2d26a
Rodnoy Konstantin <kotrod@gmail.com>
2019-10-07 15:44:56 +0300

    admin can remove user

--- web/src/main/java/com/github/cotrod/hotel/web/filter/AdminRoleFilter.java
+++ web/src/main/java/com/github/cotrod/hotel/web/filter/AdminRoleFilter.java
@@ -4,11 +4,14 @@ import com.github.cotrod.hotel.model.Role;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 import static com.github.cotrod.hotel.model.Role.*;
+import static com.github.cotrod.hotel.web.WebUtils.findCookie;
 import static com.github.cotrod.hotel.web.WebUtils.redirect;
 
 @WebFilter(urlPatterns = {"/userHome","/home"})
@@ -17,11 +20,16 @@ public class AdminRoleFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req= (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
-        Role role = Role.valueOf((String) req.getSession().getAttribute("role"));
-        if(role.equals(ADMIN)){
-            redirect("adminHome", req, resp);
+        Optional<Cookie> cookie = findCookie("myAppUserCookie", req);
+        if(cookie.isPresent()) {
+            Role role = Role.valueOf((String) req.getSession().getAttribute("role"));
+            if (role.equals(ADMIN)) {
+                redirect("adminHome", req, resp);
+            } else {
+                filterChain.doFilter(req, resp);
+            }
         }else {
-            filterChain.doFilter(req,resp);
+            redirect("login",req,resp);
         }
     }
 }
--- web/src/main/java/com/github/cotrod/hotel/web/filter/UserRoleFilter.java
+++ web/src/main/java/com/github/cotrod/hotel/web/filter/UserRoleFilter.java
@@ -4,11 +4,14 @@ import com.github.cotrod.hotel.model.Role;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 import static com.github.cotrod.hotel.model.Role.USER;
+import static com.github.cotrod.hotel.web.WebUtils.findCookie;
 import static com.github.cotrod.hotel.web.WebUtils.redirect;
 
 @WebFilter(urlPatterns = {"/home","/adminHome"})
@@ -17,11 +20,16 @@ public class UserRoleFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req= (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
-        Role role = Role.valueOf((String) req.getSession().getAttribute("role"));
-        if(role.equals(USER)){
-            redirect("userHome", req, resp);
+        Optional<Cookie> cookie = findCookie("myAppUserCookie", req);
+        if(cookie.isPresent()) {
+            Role role = Role.valueOf((String) req.getSession().getAttribute("role"));
+            if (role.equals(USER)) {
+                redirect("userHome", req, resp);
+            } else {
+                filterChain.doFilter(req, resp);
+            }
         }else {
-            filterChain.doFilter(req,resp);
+            redirect("login",req,resp);
         }
     }
 }
